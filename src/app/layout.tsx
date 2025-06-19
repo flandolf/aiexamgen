b7f2cd3fc509aad974942d5050a88081bcf022eb
flandolf <66240551+flandolf@users.noreply.github.com>
2025-06-19 21:07:04 +1000

    Add favicon to site and update layout to include it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={inter.className}>
       <head />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>
         <ThemeProvider
           attribute="class"
